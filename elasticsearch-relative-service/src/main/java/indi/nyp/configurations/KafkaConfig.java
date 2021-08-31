03fe68fd17d829867e8309a22668344f1c2f14be
牛云鹏 <niuyp0@spdb.com.cn>
2021-08-31 22:27:03 +0800

    minor

--- elasticsearch-relative-service/src/main/java/indi/nyp/configurations/KafkaConfig.java
+++ elasticsearch-relative-service/src/main/java/indi/nyp/configurations/KafkaConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.Properties;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class KafkaConfig {
     @Bean(value = "kafka consumer")
     public Properties getConsumerConfig() {
